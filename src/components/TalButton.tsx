d034b1278e18396c074a4300fb82c4a8fbc6bf20
Talisson-matos <talissonmatos23@gmail.com>
2025-01-18 15:16:26 -0300

    alterando variaveis em talButton

--- src/components/TalButton.tsx
+++ src/components/TalButton.tsx
@@ -1,23 +1,25 @@
 import React from "react"
 import styled from "styled-components"
 
-const CustomButton = styled.button`
-    background-color:indigo;
-    color:white;
+export interface TalButtonProps{
+    texto?:string
+    tipo?:'primario'|'secundario'
+    onClick?:()=>void
+}
+
+const CustomButton = styled.button<TalButtonProps>`
+    background-color:${(props:TalButtonProps)=> props.tipo === 'primario' ? "blue" : "purple"  };
+    color:${(props:TalButtonProps)=> props.tipo ===  'primario' ? "white" : "black"  };
     padding: 1rem 1.5rem;
     border: 2px solid indigo;
     transition:0.3s ease;
     cursor: pointer;
-    font-size:1.2em;
-   
-    &:hover{
-        background-color:transparent;
-        color: indigo;
-        border: 2px solid indigo;
-
-    }
+    font-size:1.2em;   
+    
 `
 
-export const TalButton = () =>{
-    return(<CustomButton>Clicar</CustomButton>)
+export const TalButton = ({texto,onClick,tipo = 'primario'} : TalButtonProps) =>{
+    return(
+    <CustomButton onClick={onClick} tipo={tipo}>{texto} </CustomButton>
+)
 }
--- stories/TalButton.stories.tsx
+++ stories/TalButton.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {TalButton} from "../src"
+import {TalButton, TalButtonProps} from "../src"
 import {ComponentMeta, ComponentStory} from "@storybook/react"
 
 export default {
@@ -7,6 +7,18 @@ export default {
    component: TalButton
 } as ComponentMeta<typeof TalButton>
 
-const Template: ComponentStory< typeof TalButton > = () => <TalButton/>
+const Template: ComponentStory< typeof TalButton > = (props) => <TalButton {...props}/>
 
-export const Primaria = Template.bind({})
+export const Primario = Template.bind({});
+
+Primario.args = {
+   texto: "Talbutton",
+   tipo: "primario"
+} as TalButtonProps
+
+export const Secundario = Template.bind({});
+
+Secundario.args = {
+   texto: "talbutton",
+   tipo: "secundario"
+} as TalButtonProps
